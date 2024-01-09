e52c413931c21db43032aeb805109cef36da23cf
Andrey <uniqcle@yandex.ru>
2024-01-09 14:50:35 +0700

    header & random char

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,15 @@
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+
 const App = () => {
-  return <div>app...</div>;
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <RandomChar />
+      </main>
+    </div>
+  );
 };
 
 export default App;
