b238f6d0842f500688c134228829fa3fe21143c4
Andrey <uniqcle@yandex.ru>
2024-01-12 10:55:42 +0700

    предохранители

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -15,7 +15,6 @@ class CharList extends Component {
   marvelService = new MarvelService();
 
   componentDidMount() {
-    console.log("did mount");
     this.marvelService
       .getAllCharacters()
       .then(this.onCharListLoaded)
